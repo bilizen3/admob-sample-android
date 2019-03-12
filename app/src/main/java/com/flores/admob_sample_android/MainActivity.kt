85a50fefab7800efdbaf4abb99ef6b3713daeabc
Bill Flores <bilizen3@gmail.com>
2019-03-12 00:23:10 -0500

    intersticial

--- app/src/main/java/com/flores/admob_sample_android/MainActivity.kt
+++ app/src/main/java/com/flores/admob_sample_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.flores.admob_sample_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,13 +12,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MobileAds.initialize(this,getString(R.string.admob_app_id))
+        MobileAds.initialize(this, getString(R.string.admob_app_id))
+
+        val adRequest = AdRequest.Builder()
+            .addTestDevice(getString(R.string.test_device))
+            .build()
+
+        val interstitialAd = InterstitialAd(this)
+        interstitialAd.adUnitId = getString(R.string.intersticial_id)
+        interstitialAd.loadAd(adRequest)
 
         btnBanner.setOnClickListener {
-            val adRequest = AdRequest.Builder()
-                .addTestDevice(getString(R.string.test_device))
-                .build()
             avBanner.loadAd(adRequest)
+        }
+
+        btnIntersticial.setOnClickListener {
+            if (interstitialAd.isLoaded) interstitialAd.show()
         }
     }
 }
