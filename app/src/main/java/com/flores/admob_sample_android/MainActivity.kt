954bf73b053e291693fff68525e25e2fd8ca26de
Bill Flores <bilizen3@gmail.com>
2019-03-11 23:51:16 -0500

    add banner

--- app/src/main/java/com/flores/admob_sample_android/MainActivity.kt
+++ app/src/main/java/com/flores/admob_sample_android/MainActivity.kt
@@ -2,11 +2,22 @@ package com.flores.admob_sample_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this,getString(R.string.admob_app_id))
+
+        btnBanner.setOnClickListener {
+            val adRequest = AdRequest.Builder()
+                .addTestDevice(getString(R.string.test_device))
+                .build()
+            avBanner.loadAd(adRequest)
+        }
     }
 }
